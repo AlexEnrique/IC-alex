8e33ea66203f35b4ce47f289fc2f3a353161e133
Alex Enrique <alex@alex-enrique.alex-enrique>
2018-07-19 00:02:12 -0300

    RNG and func. hearders add to main

--- main.c
+++ main.c
@@ -9,7 +9,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <gsl/gsl_rng.h>
+#include "functions.h"
+#include "random_generator.h"
 
 // Move this defs to a separated file after
 #define MAX_MC_LOOPS 10000
