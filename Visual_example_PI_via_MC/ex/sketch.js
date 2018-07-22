049b9d5b84bd3b342b5ab8034ae7d4d7b6c22c4b
Alex Enrique <41076109+AlexEnrique@users.noreply.github.com>
2018-07-22 12:59:43 -0300

    Update sketch.js

--- Visual_example_PI_via_MC/ex/sketch.js
+++ Visual_example_PI_via_MC/ex/sketch.js
@@ -5,29 +5,28 @@ function setup() {
   createCanvas(hw,hw);
   background(100);
 
+  var color1 = color(0, 126, 255, 5);
+  var color2 = color(0, 255, 70, 100);
 }
 
 function draw() {
-  let seed = 31415926
   let radius = height;
   let maxiterations = 1000;
 
   translate(width/2, height/2);
   noStroke();
-  var c = color(0, 126, 255, 5);
-  fill(c)
+  fill(color1)
   ellipse(0, 0, radius);
 
-  var x = random(-height, height+1);
-  var y = random(-height, height+1);
+  var x = random(-height, height+0.0000001); // [-height, height+1)
+  var y = random(-height, height+0.0000001);
 
   if (x**2 + y**2 <= radius**2) {
     m += 1;
   }
   n += 1;
 
-  c = color(0, 255, 70, 100);
-  fill(c);
+  fill(color2);
   ellipse(x, y, 3);
 
   console.log("PI: "+4*m/n);
